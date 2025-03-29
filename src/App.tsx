4aeab185f835890bcc363889ad61a359f5e3aba4
Abraham Sackey Ohene Gyan <ohenegyanfamily@gmail.com>
2025-03-29 19:48:21 +0000

    git add .

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const App = () => {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            {/* ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
